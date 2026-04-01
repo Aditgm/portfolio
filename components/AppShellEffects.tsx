4c4f1bf6fa151ef54f49c4eb85db145918e2fdc8
Aditgm <adityagithubgm@gmail.com>
2026-04-01 15:07:03 +0530

    chore: update app shell effects

--- components/AppShellEffects.tsx
+++ components/AppShellEffects.tsx
@@ -2,12 +2,16 @@
 
 import Preloader from "./Preloader";
 import CustomCursor from "./CustomCursor";
+import FluidGradientBackground from "./FluidGradientBackground";
+import { LiquidOverlay } from "./LiquidOverlay";
 
 export default function AppShellEffects() {
   return (
     <>
       <Preloader />
       <CustomCursor />
+      <FluidGradientBackground />
+      <LiquidOverlay />
     </>
   );
 }
